78f32de16666ae6d558a7074b0b79887aeee201e
michuNEEO <michael@neeo.com>
2020-04-15 22:09:42 +0200

    there is an image

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const Emulator = require('wpc-emu');
+const Jimp = require('jimp');
 
 const GAME_TO_LOAD = 'WPC-Fliptronics: Fish Tales';
 //const GAME_TO_LOAD = 'WPC-95: Attack from Mars';
 const CPU_STEPS = 16;
+
+const DMD_WIDTH = 128;
+const DMD_HEIGHT = 32;
 
 function loadFile(fileName) {
   return new Promise((resolve, reject) => {
@@ -31,14 +35,38 @@ function bootEmu() {
 }
 
 function grabDMDFrame(wpcSystem) {
-
   const ticks = parseInt(Math.random() * 5000000, 10);
   wpcSystem.executeCycle(ticks, CPU_STEPS);
 
   const frame = wpcSystem.getState().asic.display.dmdShadedBuffer;
   //TODO bail out if image is boring
 
+  savePng(frame)
   console.log('wpcSystem.getState().display.dmdShadedBuffer', frame);
+}
+
+function savePng(data) {
+  const PALETTE_R = [0, 43, 182, 254];
+  const PALETTE_G = [0, 62, 155, 255];
+  const PALETTE_B = [0, 67,  93, 211];
+  new Jimp(DMD_WIDTH, DMD_HEIGHT, '#000000', (err, image) => {
+    if (err) {
+      throw err;
+    }
+    let srcOffset = 0;
+    let dstOffset = 0;
+    for (let y = 0; y < DMD_HEIGHT; y++) {
+      for (let x = 0; x < DMD_WIDTH; x++) {
+
+        const color = data[srcOffset++];
+        image.bitmap.data[dstOffset++] = PALETTE_R[color];
+        image.bitmap.data[dstOffset++] = PALETTE_G[color];
+        image.bitmap.data[dstOffset++] = PALETTE_B[color];
+        image.bitmap.data[dstOffset++] = 255;
+      }
+    }
+    image.write('./out.png');
+  });
 }
 
 bootEmu()
